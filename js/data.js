feaceddfaceae42a823a4529910c97530f5e1faf
@jeya <ajeya@ormae.com>
2022-03-03 13:20:23 +0530

    example question using array methods

---
+++ js/data.js
@@ -0,0 +1,147 @@
+//we will store data in dbs  (excel file)
+//information about single entity will be stored in single table / (sheet)
+//to fetch data from db , we need backend with the help of APIs
+
+let users = [
+  {
+    userId: 'u001',
+    name: 'Aravind',
+    city: 'Pune',
+    carsOwned: 1, //['1', '3', '7'],
+  },
+  {
+    userId: 'u002',
+    name: 'Kamal',
+    city: 'Goa',
+    carsOwned: 3, //['2', '8', '3'],
+  },
+  {
+    userId: 'u003',
+    name: 'Rani',
+    city: 'Kerala',
+    carsOwned: 5, //['5'],
+  },
+  {
+    userId: 'u004',
+    name: 'Suresh',
+    city: 'Karnataka',
+    carsOwned: 6, //['4', '6'],
+  },
+]
+
+let cars = [
+  {
+    carId: 1,
+    carName: 'Audi e-tron',
+    type: 'electric',
+    priceInLacks: 100,
+    imgUrl:
+      'https://media.zigcdn.com/media/model/2021/Jul/auti-etron-2_360x240.jpg',
+    taxInPercentage: 7,
+  },
+  {
+    carId: 2,
+    carName: 'Hyundai Kona Electric',
+    type: 'electric',
+    priceInLacks: 23.79,
+    imgUrl:
+      'https://media.zigcdn.com/media/model/2020/Jun/hyundai-kona_360x240.jpg',
+    taxInPercentage: 7,
+  },
+  {
+    carId: 3,
+    carName: 'Mg ZS EV',
+    type: 'electric',
+    priceInLacks: 21.49,
+    imgUrl:
+      'https://media.zigcdn.com/media/model/2020/Jun/mg-zs-ev_360x240.jpg',
+    taxInPercentage: 7,
+  },
+  {
+    carId: 4,
+    carName: 'Kia Seltos',
+    type: 'fuel',
+    priceInLacks: 9.95,
+    imgUrl:
+      'https://media.zigcdn.com/media/model/2021/May/seltos-3_360x240.jpg',
+    taxInPercentage: 12,
+  },
+  {
+    carId: 5,
+    carName: 'Mahendra XUV700',
+    type: 'fuel',
+    priceInLacks: 12.95,
+    imgUrl:
+      'https://media.zigcdn.com/media/model/2021/Oct/xuv700-5_360x240.jpg',
+    taxInPercentage: 12,
+  },
+  {
+    carId: 6,
+    carName: 'Citroen C5 Aircross',
+    type: 'fuel',
+    priceInLacks: 32.23,
+    imgUrl:
+      'https://media.zigcdn.com/media/model/2021/Feb/c5-aircoss-2_360x240.jpg',
+    taxInPercentage: 12,
+  },
+  {
+    carId: 7,
+    carName: 'Maruti Wagon R',
+    type: 'cng',
+    priceInLacks: 5.39,
+    imgUrl:
+      'https://media.zigcdn.com/media/model/2022/Feb/wagonr-5_360x240.jpg',
+    taxInPercentage: 10,
+  },
+  {
+    carId: 8,
+    carName: 'Tata Tiago',
+    type: 'cng',
+    priceInLacks: 5.19,
+    imgUrl: 'https://media.zigcdn.com/media/model/2021/Apr/tiago-3_360x240.jpg',
+    taxInPercentage: 10,
+  },
+]
+
+// for (let user in users) {
+//   let userName = user.name
+//   let userCity = user.city
+//   let carsOwnedByUser = user.carsOwned
+
+//   console.log(userName, '---------')
+//   console.log(carsOwnedByUser)
+// }
+
+let userAndCarInfo = []
+
+users.forEach((user) => {
+  //console.log(user)
+  //   console.log(userName, '---------')
+  //   console.log(carsOwnedByUser)
+
+  let carDetails = cars.filter((car) => {
+    return car.carId === user.carsOwned
+  })
+
+  let newUserCarInfo = {
+    name: user.name,
+    city: user.city,
+    carsOwnedByUser: carDetails,
+  }
+
+  userAndCarInfo.push(newUserCarInfo)
+})
+
+console.log(userAndCarInfo)
+// let arr = [2, 5, 6, 8, 11]
+// let result = arr.filter((item) => {
+//   return item % 2 === 0
+// })
+
+// console.log(result)
+
+// 2 % 2 =0 === 0 ==> true
+// 5 % 2 = 1 === 0 ==> false
+// 6 % 2 = 0 === 0 ==> true
+// 8 % 2 = 0 ==== 0 ==> true
+// 11 % 2 = 1 === 0 ==> false
--- js/js.function.js
+++ js/js.function.js
@@ -7,46 +7,81 @@ let arr6 = [8, 2, 52, 6, 9, 2]
 
 //functions ==> function keyword , function name, (arguments), curly braces
 function addArrayItemsV1(arr, initialValue) {
-	let sum = initialValue
-	for (let i = 0; i < arr.length; i++) {
-		//newvalue= prev value + current value
-		sum = sum + arr[i]
-	}
-	console.log('sum--', sum)
+  let sum = initialValue
+  for (let i = 0; i < arr.length; i++) {
+    //newvalue= prev value + current value
+    sum = sum + arr[i]
+  }
+  console.log('sum--', sum)
 }
 
 let addArrayItemsV2 = function (arr, initialValue) {
-	let sum = initialValue
-	for (let i = 0; i < arr.length; i++) {
-		//newvalue= prev value + current value
-		sum = sum + arr[i]
-	}
-	console.log('sum--', sum)
+  let sum = initialValue
+  for (let i = 0; i < arr.length; i++) {
+    //newvalue= prev value + current value
+    sum = sum + arr[i]
+  }
+  console.log('sum--', sum)
 }
 
 let addArrayItemsV3 = (arr, initialValue) => {
-	let sum = initialValue
-	for (let i = 0; i < arr.length; i++) {
-		//newvalue= prev value + current value
-		sum = sum + arr[i]
-	}
-	console.log('sum--', sum)
+  let sum = initialValue
+  for (let i = 0; i < arr.length; i++) {
+    //newvalue= prev value + current value
+    sum = sum + arr[i]
+  }
+  console.log('sum--', sum)
 }
 
-;((arr, initialValue) => {
-	let sum = initialValue
-	for (let i = 0; i < arr.length; i++) {
-		//newvalue= prev value + current value
-		sum = sum + arr[i]
-	}
-	console.log('sum--', sum)
-})(arr1, 200)
+// ;((arr, initialValue) => {
+//   let sum = initialValue
+//   for (let i = 0; i < arr.length; i++) {
+//     //newvalue= prev value + current value
+//     sum = sum + arr[i]
+//   }
+//   console.log('sum--', sum)
+// })(arr1, 200)
 
 //var let const
-// addArrayItemsV3(arr1, 200)
+// let returnValue = addArrayItemsV1(arr1, 100)
 
 //primitive Types - numbers, strings, boolean, undefined, null, symbol
 //non-primitive types - array, objects , set
 //for loop and its varients
 //if-else chain
 //functions
+
+//add two numbers
+//divide "that answer" by 2
+let num1 = 20
+let num2 = 10
+//return 15
+
+//(num1+num2)/2
+let sum = num1 + num2
+let avg = sum / 2
+console.log('avg--', avg)
+
+function addTwoNumbers(num1, num2) {
+  let sum = num1 + num2
+  return sum
+}
+
+function avgOfTwoValues(num1, num2) {
+  // calling one function within another function
+  let avg = addTwoNumbers(num1, num2) / 2
+  return avg
+}
+
+function avgOfTwoValues2(num1, num2, addFunction) {
+  // passing a function as an argument/paramter to another function ==> callback function
+  console.log('addFunction--', addFunction)
+  let avg = addFunction(num1, num2) / 2
+  return avg
+}
+
+// let resultSum = addTwoNumbers(10, 20)
+let resultAvg = avgOfTwoValues2(10, 20, addTwoNumbers)
+
+//function call/invocation
+console.log('resultAvg--', resultAvg)
